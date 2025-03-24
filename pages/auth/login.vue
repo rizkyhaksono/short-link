eafcf2112ce07bf64edee07c85c1bd85b2d3f9fd
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-03-25 01:28:16 +0700

    feat(auth): login and register page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,8 +1,75 @@
-<script setup>
+<script lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const password = ref('');
+const loading = ref(false);
+const errorMessage = ref('');
+const router = useRouter();
+
+const login = async () => {
+  loading.value = true;
+  errorMessage.value = '';
+
+  try {
+    const response = await $fetch('http://api.link.natee.me/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: { email: email.value, password: password.value }
+    });
+
+    console.log('Login Success:', response);
+    // document.cookie = `auth_link=${response?.token}; path=/; max-age=86400`;
+    // router.push('/dashboard');
+  } catch (error) {
+    errorMessage.value = error || 'Login failed. Please try again.';
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<script setup lang="ts">
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
 </script>
 
 <template>
-  <LayoutsAuth>
-    Login
-  </LayoutsAuth>
+  <div class="min-h-screen flex items-center justify-center">
+    <Card class="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl">
+          Login
+        </CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account.
+        </CardDescription>
+      </CardHeader>
+      <CardContent class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button class="w-full">
+          Sign in
+        </Button>
+
+      </CardFooter>
+      <div class="mb-4 text-center text-sm">
+        Don't have an account?
+        <NuxtLink href="/auth/register" class="underline">
+          Sign up
+        </NuxtLink>
+      </div>
+    </Card>
+  </div>
 </template>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,8 +1,59 @@
-<script setup>
+<script lang="ts">
+export const description
+  = 'A sign up form with first name, last name, email and password inside a card. There\'s an option to sign up with GitHub and a link to login if you already have an account'
+export const iframeHeight = '600px'
+export const containerClass = 'w-full h-screen flex items-center justify-center px-4'
+</script>
+
+<script setup lang="ts">
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
 </script>
 
 <template>
-  <LayoutsAuth>
-    Register
-  </LayoutsAuth>
+  <div class="min-h-screen flex items-center justify-center">
+    <Card class="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-xl">
+          Sign Up
+        </CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-4">
+          <div class="grid grid-cols-2 gap-4">
+            <div class="grid gap-2">
+              <Label for="first-name">First name</Label>
+              <Input id="first-name" placeholder="Max" required />
+            </div>
+            <div class="grid gap-2">
+              <Label for="last-name">Last name</Label>
+              <Input id="last-name" placeholder="Robinson" required />
+            </div>
+          </div>
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" required />
+          </div>
+          <div class="grid gap-2">
+            <Label for="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button type="submit" class="w-full">
+            Create an account
+          </Button>
+        </div>
+        <div class="mt-4 text-center text-sm">
+          Already have an account?
+          <NuxtLink href="/auth/login" class="underline">
+            Sign in
+          </NuxtLink>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
 </template>
